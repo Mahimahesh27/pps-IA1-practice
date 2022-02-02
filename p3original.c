eae199781ab3f9a062451e6cb3282827137db9a9
Mahesh <94962227+Mahimahesh27@users.noreply.github.com>
2022-02-02 11:03:24 +0000

    p3

---
+++ p3original.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int input();
+int sum_of_n_numbers(int n);
+void output(int n, int sum);
+
+int main()
+{
+    int n = input();
+    int ans = sum_of_n_numbers(n);
+    
+    output(n, ans);
+    //    sum = n*(n + 1)/2;
+    
+   return 0;
+}
+
+int input(int x)
+{
+    printf("Enter number n = ");
+    scanf("%d", &x);
+    return x;
+}
+
+int sum_of_n_numbers(int n)
+{
+  int i = 1;
+  int sum = 0;
+  while (i <= n)
+    {
+      sum += i;
+      ++i;
+    }
+  return sum;
+}
+
+void output(int n, int sum)
+{
+  printf("Sum of %d natural numbers = %d", n, sum);
+}
